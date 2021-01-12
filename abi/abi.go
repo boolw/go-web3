fd4fe1947a3f60a3c39cb9a77e0ff8cb7ac17280
zm <zm@zm.com>
2021-01-12 11:20:22 +0800

    方法签名判断错误

--- abi/abi.go
+++ abi/abi.go
@@ -120,7 +120,7 @@ func (m *Method) Sig() string {
 }
 
 func (m *Method) MethodSig() string {
-	if len(m.Outputs.tuple) > 0 {
+	if len(m.Outputs.tuple) == 0 {
 		return m.Sig()
 	}
 	return fmt.Sprintf("%s %s", m.Sig(), buildSignature("returns ", m.Outputs))
